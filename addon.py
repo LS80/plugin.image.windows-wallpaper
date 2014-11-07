9f8a3ba2b0da211f0cc21078f3b7c223e4c7b287
LS80 <github@lee-smith.me.uk>
2014-11-07 07:50:15 +0000

    Support a network location for downloads by using xbmcvfs.

--- addon.py
+++ addon.py
@@ -23,11 +23,16 @@ import os
 import sys
 import re
 import urllib
+import urllib2
 from urlparse import urljoin, urlsplit, urlunsplit
 
 import requests
 from bs4 import BeautifulSoup
 from xbmcswift2 import Plugin, xbmcgui
+try:
+    import xbmcvfs
+except ImportError:
+    from xbmcswift2 import xbmcvfs
 
 
 BASE_URL = "http://windows.microsoft.com/en-GB/windows/wallpaper"
@@ -76,6 +81,53 @@ def get_items(category_id):
 
         yield item
 
+
+class DownloadError(Exception):
+    pass
+
+
+class DownloadProgress(object):
+
+    BLOCK_SIZE = 131072
+
+    def __init__(self, heading, remote, outpath, size):
+        self._heading = heading
+        self._remote = remote
+        self._outpath = outpath
+        self._outfile = os.path.basename(outpath)
+        self._out = None
+        
+        self._size = size
+        self._progress = xbmcgui.DialogProgressBG()  
+        self._done = 0
+ 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._remote.close()
+        if self._out is not None:
+            self._out.close()
+
+        self._progress.close()
+
+        if exc_type is not None:
+            xbmcvfs.delete(self._outpath)
+
+    def start(self):
+        self._progress.create(self._heading, self._outfile)
+        self._out = xbmcvfs.File(self._outpath, 'w')
+
+        while self._done < self._size:
+            data = self._remote.read(self.BLOCK_SIZE)
+            self._done += len(data)
+            result = self._out.write(data)
+            if not result:
+                raise DownloadError("Unable to download image")
+            percent = int(self._done * 100 / self._size)
+            self._progress.update(percent)
+
+
 class DownloadImage(object):
     def __init__(self, url, filename):
         directory = plugin.get_setting('directory')
@@ -86,15 +138,20 @@ class DownloadImage(object):
         if directory:
             path = os.path.join(directory, filename)
             url = urllib.unquote(url)
-            self.progress = xbmcgui.DialogProgressBG()
-            self.progress.create(plugin.get_string(30020), filename)
-            urllib.urlretrieve(url, path, self.update)
-        else:
-            xbmcgui.Dialog().ok(plugin.get_string(30000), plugin.get_string(30021))
-
-    def update(self, nblocks, block_size, file_size):
-        percent = int(nblocks * block_size * 100 / file_size)
-        self.progress.update(percent)
+            try:
+                response = requests.get(url, stream=True)
+                size = int(response.headers["content-length"])
+            except Exception as e:
+                xbmcgui.Dialog().ok(plugin.get_string(30022), str(e), url)
+            else:
+                try:
+                    with DownloadProgress(plugin.get_string(30020),
+                                          response.raw, path, size) as downloader:
+                        downloader.start()
+                except WriteError as e:
+                    xbmcgui.Dialog().ok(plugin.get_string(30022), str(e), path,
+                                        plugin.get_string(30023))
+                    plugin.open_settings()
 
 
 @plugin.route('/')
